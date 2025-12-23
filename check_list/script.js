ed0d43b7ebb794ad4abc971fdea5a5efdc54cbcb
Lenizio27 <155769957+Lenizio27@users.noreply.github.com>
2025-12-23 20:44:19 -0300

    check_list: inicio codigos timer.
    dado inicio aos codigos para o timerFocus, criaçãou de layout e estilos.

--- check_list/script.js
+++ check_list/script.js
@@ -1,7 +1,24 @@
 let btFocus = document.getElementById("btFocus");
-let timerFocus = 25
-function periodoConcentracao(){
-    let timer = document.getElementById("timer");
-    timer.textContent = 10
+let btLeft = document.getElementById("btLeft");
+let btRight = document.getElementById("btRight");
+let timerFocus = document.getElementById("timer");
+let timeout = document.getElementById("timeout");
+let Focus = 25;
+
+// Function Focus
+btFocus.addEventListener("click", ()=>{
+})
+
+function pushTimer(timer){
+    timer === `+` ? Focus += 25 : Focus -= 25;
+    if(Focus < 25){
+        Focus += 25;
+        return
+    }
+    let timeoutValue = Math.ceil(Focus / 50);
+    timerFocus.textContent = Focus;
+    timeout.textContent = timeoutValue + ` pausas`
 }
-btFocus.addEventListener("click", periodoConcentracao)
+
+btRight.addEventListener("click", ()=> pushTimer("+"));
+btLeft.addEventListener("click", ()=> pushTimer("-"));
